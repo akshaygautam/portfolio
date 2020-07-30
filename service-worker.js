bdc2ff288153549ef91f225b824ddbdae0e900ee
Akshay Gautam <akshay.gautam2209@gmail.com>
2020-07-30 17:20:09 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.38089affd0278f80fdd3d700de790ba3.js"
+  "/portfolio/precache-manifest.a2e84c5c5d3a98943967ebaca61f726f.js"
 );
 
 self.addEventListener('message', (event) => {
