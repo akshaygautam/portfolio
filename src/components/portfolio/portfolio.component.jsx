f0b5ef7cfe743efaedf3ba9fd706e2a317ddb9ab
Akshay Gautam <akshay.gautam2209@gmail.com>
2020-07-30 12:58:08 +0530

    next tab link

--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -4,7 +4,7 @@ import "./portfolio.styles.css";
 export const Portfolio = ({ content }) => {
   return (
     <div className="portfolio-container">
-      <a href={content.siteURL}>
+      <a href={content.siteURL} target="_blank" rel="noopner noreferrer">
         <img alt="monster" src={content.imageURL} />
       </a>
       <h3>{content.displayInfo}</h3>
