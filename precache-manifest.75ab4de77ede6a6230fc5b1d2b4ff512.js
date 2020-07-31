27e1b8d6e2efae8849af2c6266b733eba03772a3
Akshay Gautam <akshay.gautam2209@gmail.com>
2020-08-01 03:09:24 +0530

    Updates

---
+++ precache-manifest.75ab4de77ede6a6230fc5b1d2b4ff512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4385389a86f6be13aa19d37aa78e11",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b4eafa31a72fc1b5ef72",
+    "url": "/portfolio/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "9066864d5dcddb36f65f",
+    "url": "/portfolio/static/css/main.d17277b8.chunk.css"
+  },
+  {
+    "revision": "b4eafa31a72fc1b5ef72",
+    "url": "/portfolio/static/js/2.20934309.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/portfolio/static/js/2.20934309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9066864d5dcddb36f65f",
+    "url": "/portfolio/static/js/main.d675dd36.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
